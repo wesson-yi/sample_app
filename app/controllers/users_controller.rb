89875505e4f60fc0a45661a91166da78532672b5
wesson.yi <wesson.yi@wq-wesson-yi.local>
2016-10-09 18:45:54 +0800

    Actions of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ require 'will_paginate'
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page])
